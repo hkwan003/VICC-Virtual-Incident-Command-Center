587475816ecc33cdae80aa4d2a2ab3415503f170
Calvin Kwan <hkwan003@ucr.edu>
2015-07-15 21:23:42 -0700

    Initial commit

---
+++ MessagingTutorial/src/main/java/com/sinch/messagingtutorial/app/MyApplication.java
@@ -0,0 +1,13 @@
+package com.sinch.messagingtutorial.app;
+
+import android.app.Application;
+import com.parse.Parse;
+
+public class MyApplication extends Application
+{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.initialize(this, "vnCbnf4r1MiM0EPAgCDuddmeDMEKz46hVpBVSwSv", "lKFrjBuEyn59KVBJuvUJNSdVDweAkfXznH5lEJxx");
+    }
+}
